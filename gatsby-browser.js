3db5e0673db5cc9c025e6b05de30d101a37fefb8
nasa1515 <h43254@gmail.com>
2021-07-15 14:11:35 +0900

    Commit

--- gatsby-browser.js
+++
@@ -1,5 +0,0 @@
-import '@fontsource/montserrat';
-import './src/styles/_reset.scss';
-import './src/styles/_global-style.scss';
-import './src/styles/_markdown-style.scss';
-import 'prismjs/themes/prism-tomorrow.css';
